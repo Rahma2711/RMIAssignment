db95ec40634933d0145d81c5f9a0ac9feb6f61ab
Rahma2711 <79481555+Rahma2711@users.noreply.github.com>
2021-12-12 22:37:09 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,3 @@
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-
 public class Server extends UnicastRemoteObject implements MyInterface {
 
     public Server() throws RemoteException {}
@@ -14,4 +9,36 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+    @Override
+    public String  reverse(String R) {
+        return new StringBuilder(R).reverse().toString();
+}
+    @Override
+    public char MinChar(String R){
+        char ch =R.charAt(0);
+        for (int i = 1; i <R.length()  ; i++) {
+            if(R.charAt(i) < ch){
+                ch=R.charAt(i);
+            }
+        }
+        return ch;
+    }
+    @Override
+    public String CaseChanger(String R){
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < R.length() ; i++) {
+            if(R.charAt(i) >='a' && R.charAt(i) <='z'){
+                res.append((char)(R.charAt(i) - 32));
+
+            }else if(R.charAt(i) >='A' && R.charAt(i) <='Z'){
+                res.append((char)(R.charAt(i) + 32));
+
+            }else{
+                res.append(R.charAt(i));
+            }
+        }
+
+        return res.toString();
+    }
+
 }
